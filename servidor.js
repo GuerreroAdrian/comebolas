65ba47845065712694951f3a16a6403c0e433a48
GuerreroAdrian <55460997+GuerreroAdrian@users.noreply.github.com>
2019-11-21 15:02:19 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -2,7 +2,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer(function (req, res) {
-  var q = url.parse(rec.url, true).query;
+  var q = url.parse(req.url, true).query;
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola' + q.nombre);
+  res.end('Hola ' + q.nombre);
 }).listen(process.env.PORT);
