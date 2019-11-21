4ec42d91dd9ce83e73fed2a7ba50dfc13469c6fe
GuerreroAdrian <55460997+GuerreroAdrian@users.noreply.github.com>
2019-11-21 15:16:58 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,11 +1,15 @@
 var http = require('http');
 var url = require('url');
 
-var nombres = [];
+var jugadores = [];
 
 http.createServer(function (req, res) {
-  var q = url.parse(req.url, true).query;
-  nombres.push(q.nombre);
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Lista de Nombres ' + nombres.join());
+  var q = url.parse(req.url, true).query;
+  
+  if(JSON.parse(q.jugador)){
+    res.end('Interpretado');
+  } else {
+     res.end('Lista de Nombres ' + nombres.join());
+  } 
 }).listen(process.env.PORT);
