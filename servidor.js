8513cd3e95d9cc9905d45bc2dd04ac531a80aa64
GuerreroAdrian <55460997+GuerreroAdrian@users.noreply.github.com>
2019-11-21 16:11:50 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -6,32 +6,32 @@ var jugadores = [];
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   var q = url.parse(req.url, true).query;
-  
+
   if(q.jugador){
     var jugador = JSON.parse(q.jugador);
-    if(typeof jugador.nom === 'string' && 
-       typeof jugador.pos === 'object' && 
-       typeof jugador.pos.length === 'number' && 
+    if(typeof jugador.nom === 'string' &&
+       typeof jugador.pos === 'object' &&
+       typeof jugador.pos.length === 'number' &&
        jugador.pos.length === 2 &&
        typeof jugador.tam === 'number'){
         var indice = -1;
-      for(var i = 0; i < jugadores.length; i++){
-        if(jugadores[i].nom === jugador.nom){
-          indice = i;
-          break;
+        for(var i = 0; i < jugadores.length; i++){
+          if(jugadores[i].nom === jugador.nom){
+            indice = i;
+            break;
+          }
         }
-      }
-      if(indice < 0){
-        jugadores.push(jugador);
-        res.end('Jugador Creado');
-      } else {
-        jugadores[indice] = jugador;
-        res.end('Jugador Actualizado');
-      }
-       } else {
-        res.end('incorrecto');
-       }
+        if(indice < 0){
+          jugadores.push(jugador);
+          res.end('Jugador Creado');
+        } else {
+          jugadores[indice] = jugador;
+          res.end('Jugador Actualizado');
+        }
+    } else {
+      res.end('incorrecto');
+    }
   } else {
      res.end('Error');
-  } 
+  }
 }).listen(process.env.PORT);
