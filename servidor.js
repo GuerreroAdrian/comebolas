79a2832acb5634b2a2b75c0e14e451be4b16f1cb
GuerreroAdrian <55460997+GuerreroAdrian@users.noreply.github.com>
2019-11-21 14:51:52 -0600

    Update servidor.js

--- servidor.js
+++ servidor.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('<b>Hola</b>');
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end('Hola este es el servidor de <b>Adrian</b>!');
 }).listen(process.env.PORT);
